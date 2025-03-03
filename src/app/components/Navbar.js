ba20a6d65b0a515699231e72fd28c861e0acfcb9
rexwithluv <aaronaax@gmail.com>
2025-03-03 15:32:17 +0100

    ahora existe un componente global header

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,53 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-primary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" href={"/"}>
+          TTPS
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" href={"/"}>
+                Inicio
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" href={"/"}>
+                Tienda
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" href={"/"}>
+                Sobre nosotros
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" href={"/"}>
+                Contacto
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,21 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+"use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import "./globals.scss";
+import { useEffect } from "react";
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return (
     <html lang="es-ES">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
