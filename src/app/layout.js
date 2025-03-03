e7b0a91a2f640de3767d8ec9e9c7c2619f07d232
rexwithluv <aaronaax@gmail.com>
2025-03-03 16:36:37 +0100

    añadidos iconso

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,13 @@
 "use client";
 
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import { useEffect } from "react";
+
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+
 import "./globals.scss";
 
 export default function RootLayout({ children }) {
