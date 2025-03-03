f32b85e2809ad7cbd095c6151d2b8ab21b85f203
rexwithluv <aaronaax@gmail.com>
2025-03-03 16:04:23 +0100

    created footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,22 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="bg-dark text-white text-center pb-2 pt-3">
+      <div className="container">
+        <p>&copy; {currentYear} The Tortured Poets Store.</p>
+        <Link href={"/politica-privacidad"} className="text-white">
+          Política de privacidad
+        </Link>
+        &nbsp;|&nbsp;
+        <Link href={"/terminos-uso"} className="text-white">
+          Términos de uso
+        </Link>
+      </div>
+    </footer>
+  );
+}
