12e0d1f705470835f29bcd3fea3698421682b385
rexwithluv <aaronaax@gmail.com>
2025-03-03 19:03:18 +0100

    en la pagina principal hay una imagen mientras no se me ocurre que poner

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-      <p>It&apos;s me, hi!</p>
-    </div>
+    <>
+      <Image
+        src="/images/ttps-main.png"
+        alt="Main image of the store"
+        layout="responsive"
+        height={1820}
+        width={1080}
+      />
+    </>
   );
 }
